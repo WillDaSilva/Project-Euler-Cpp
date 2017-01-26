1feeb08ef26fd22eddd68be5b8eef073261218d7
Will Da Silva <will@willdasilva.xyz>
2017-01-26 01:28:44 -0500

    Solution to problem 5

---
+++ 5.cpp
@@ -0,0 +1,22 @@
+#include "toolkit.h"
+
+int main() {
+
+    int n = 0;
+    bool found;
+    while (true) {
+        n++;
+        found = true;
+        for (int i = 1; i < 21; i++) {
+            if (n % i != 0) {
+                found = false;
+            }
+        }
+        if (found) {
+            std::cout << n << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
