063dedf77ff68e2e7e60b200ed687c7dd724aa88
Will Da Silva <will@willdasilva.xyz>
2017-01-26 01:41:09 -0500

    Solution to problem 6

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+#include "toolkit.h"
+
+int main() {
+
+    unsigned long long sumOfSquares = 0;
+    unsigned long long squareOfSums = 0;
+
+    for (int i = 1; i < 101; i++) {
+        sumOfSquares += (int)std::floor(std::pow(i, 2));
+        squareOfSums += i;
+    }
+
+    std::cout << (unsigned long long)std::floor(std::pow(squareOfSums, 2)) - sumOfSquares << std::endl;
+    
+    return 0;
+}
