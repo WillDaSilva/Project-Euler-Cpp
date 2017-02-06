f374345d21ba2486ebfa4156b6760159725e63dd
Will Da Silva <will@willdasilva.xyz>
2017-02-05 22:39:04 -0500

    Solution to problem 12

---
+++ 12.cpp
@@ -0,0 +1,25 @@
+#include "toolkit.h"
+
+int main() {
+
+    int triangularNumber = 1;
+    int incrementer = 1;
+    int numDivisors = 0;
+
+    while (numDivisors <= 500) {
+
+        numDivisors = 0;
+        incrementer += 1;
+        triangularNumber += incrementer;
+
+        for (int i = 1; i*i <= triangularNumber; i++) {
+            if (triangularNumber % i == 0) {
+                numDivisors += 2;
+            }
+        }
+    }
+
+    std::cout << triangularNumber << std::endl;
+
+    return 0;
+}
