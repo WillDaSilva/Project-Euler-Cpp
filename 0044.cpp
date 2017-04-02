8a80f8c625ea3a36888df6c67819585e5cb95fc5
Will Da Silva <will@willdasilva.xyz>
2017-04-02 17:01:27 -0400

    Solution to problem 44

---
+++ 0044.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+unsigned long pentagonal(int n) {
+    return n * (3 * n - 1) / 2;
+}
+
+bool isPentagonal(unsigned long n) {
+    return (std::sqrt(24 * n + 1) + 1) == (unsigned long)(std::sqrt(24 * n + 1) + 1) && (unsigned long)(std::sqrt(24 * n + 1) + 1) % 6 == 0;
+}
+
+int main() {
+
+    for (int a = 1; a < 3000; a++) {
+        for (int b = a + 1; b < 3000; b++) {
+            if (isPentagonal(pentagonal(b) - pentagonal(a)) && isPentagonal(pentagonal(b) + pentagonal(a))) {
+                std::cout << pentagonal(b) - pentagonal(a) << std::endl;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
